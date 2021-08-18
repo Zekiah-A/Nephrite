fc895f7a83d578c5cfd90a58bee701d05b61f103
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2021-08-18 17:42:00 +0300

    Made program.cs as small as possible.

--- Nephrite/NephriteRunner.cs
+++ Nephrite/NephriteRunner.cs
@@ -3,12 +3,56 @@ using NephriteRunner.Lexer;
 using NephriteRunner.Runtime;
 using NephriteRunner.SyntaxAnalysis;
 using System;
+using System.IO;
 
 namespace NephriteRunner
 {
     public class NephriteRunner
     {
-        public bool Run(string source)
+        public void Run(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                try
+                {
+                    Execute(File.ReadAllText(args[0]));
+                }
+                catch (IOException)
+                {
+                    ReportError($"Couldn't open the file. ({args[0]})");
+                }
+            }
+
+            else if (args.Length == 0)
+            {
+                Console.Write($"You have entered the nephrite REPL. Enter a command to run it.\nUse ");
+                WriteConsoleColour(ConsoleColor.DarkCyan, "ctrl + c");
+                Console.Write(" or ");
+                WriteConsoleColour(ConsoleColor.DarkCyan, "exit 0;");
+                Console.Write(" to quit the repl.\n\n");
+
+                while (true)
+                {
+                    Console.Write(">> ");
+                    var input = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(input))
+                        Execute(input);
+
+                    else
+                        break;
+
+                    Console.WriteLine();
+                }
+
+            }
+
+            else
+                ReportError("Usage: Press enter to start the REPL. Or write the file path to run it.");
+
+        }
+
+        private void Execute(string source)
         {
             try
             {
@@ -20,16 +64,16 @@ namespace NephriteRunner
             catch (Exception error) when (error is ScanningErrorException || error is ParsingErrorException || error is RuntimeErrorException)
             {
                 ReportError(error.StackTrace == null ? error.Message : $"{error.Message}\n{error.StackTrace}");
-                return false;
             }
-
-            return true;
         }
 
-        public void ReportError(string message)
+        private void ReportError(string message)
+            => WriteConsoleColour(ConsoleColor.Red, message);
+
+        private static void WriteConsoleColour(ConsoleColor colour, string text)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
+            Console.ForegroundColor = colour;
+            Console.Write(text);
             Console.ResetColor();
         }
     }
--- Nephrite/Program.cs
+++ Nephrite/Program.cs
@@ -1,59 +1,8 @@
-﻿using System;
-using System.IO;
-
-namespace NephriteRunner
+﻿namespace NephriteRunner
 {
     internal class Program
     {
         private static void Main(string[] args)
-        {
-            var nephrite = new NephriteRunner();
-
-            if (args.Length == 1)
-            {
-                try
-                {
-                    nephrite.Run(File.ReadAllText(args[0]));
-                }
-                catch (IOException)
-                {
-                    nephrite.ReportError($"Couldn't open the file. ({args[0]})");
-                }
-            }
-
-            else if (args.Length == 0)
-            {
-                Console.Write($"You have entered the nephrite REPL. Enter a command to run it.\nUse ");
-                WriteConsoleColour(ConsoleColor.DarkCyan, "ctrl + c");
-                Console.Write(" or "); 
-                WriteConsoleColour(ConsoleColor.DarkCyan, "exit 0;");
-                Console.Write(" to quit the repl.\n\n");
-
-                while (true)
-                {
-                    Console.Write(">> ");
-                    var input = Console.ReadLine();
-
-                    if (!string.IsNullOrEmpty(input))
-                        new NephriteRunner().Run(input);
-                    
-                    else
-                        break;
-                }
-            }
-
-            else
-                nephrite.ReportError("Usage: Press enter to start the REPL. Or write the file path to run it.");
-
-        }
-
-        public static string WriteConsoleColour(ConsoleColor colour, string text)
-        {
-            ConsoleColor originalcolour = Console.ForegroundColor;
-            Console.ForegroundColor = colour;
-            Console.Write(text);
-            Console.ForegroundColor = originalcolour;
-            return null;
-        }
+            => new NephriteRunner().Run(args);
     }
 }
